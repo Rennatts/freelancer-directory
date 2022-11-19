b37919cd50956410914aab8a277874449f001a1c
Renata Machado <16.renata@gmail.com>
2022-11-19 18:39:37 -0300

    refactoring

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,28 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/oi",
-//     element: <Root />,
-//     errorElement: <ErrorPage />,
-//   },
-// ]);
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    {/* <RouterProvider router={router} />
-    <App /> */}
     <App/>
-    {/* <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App/>}></Route>
-        <Route path="/ola" element={<Root/>}></Route>
-      </Routes>
-    </BrowserRouter> */}
   </React.StrictMode>
 );
