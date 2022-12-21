15cb1b9cb678e1a1bb043d87b4c6300bfd2f3f74
Renata Machado <16.renata@gmail.com>
2022-12-21 18:48:17 -0300

    profile getting pretty

--- client/src/pages/Freelancer/FreelancerProfile/index.tsx
+++ client/src/pages/Freelancer/FreelancerProfile/index.tsx
@@ -40,33 +40,36 @@ export function FreelancerProfile ({ size= 'lg'}: ITFreelancerProfileProps) {
 
 
     return (
-        <div className="place-content-around p-20 ml-80 mr-80">
+        <div className="place-content-around p-20  mt-2 ml-80 mr-80 shadow-2xl">
             <div className="flex items-start flex-start flex-col">
                 <div className='w-full flex flex-row align-center justify-center text-xl'>
                   <h1 className="flex align-center">{freelancer?.job_title}</h1> 
                 </div>
-                <div className='w-full flex flex-col items-center justify-center'>
+                <div className='w-full flex flex-col items-center justify-center border-b border-teal-500'>
                     <img className='h-14 w-12' src={ProfilePhoto} alt="profile_photo"></img>
-                    <p className=''>{freelancer?.name}</p>
-                    <p className=''>
+                    <p className='mt-2'>{freelancer?.name}</p>
+                    <p className='mt-2'>
                       <FontAwesomeIcon className="text-teal-500 mr-2" icon={faLocationDot} />{freelancer?.city}
                     </p> 
-                    <p>Member since
+                    <p className='mt-2'>Member since
                         <Moment className="ml-2" format="YYYY/MM/DD">{freelancer?.createdAt}</Moment>
                     </p>
+                    <br/>
                 </div>
-                <div>
-                <h1>{freelancer?.description}</h1> 
+                <div className='w-full flex align-center justify-center mt-8 mb-8'>
+                    <h1>{freelancer?.description}</h1> 
                 </div>
                 <div>
                     <p>Contact Info</p>
                    <p>{freelancer?.email}</p> 
+                   <p>{freelancer?.instagram}</p> 
+                   <p>{freelancer?.website}</p> 
                 </div>
             </div>
 
             {context?.id === freelancer?.id ? 
             (
-                <div>
+                <div className='mt-12'>
                     <button onClick={() => navigate(`/freelancer/profile/edit/${freelancer?.id}`)} className="text-teal-500 background-transparent font-bold uppercase px-3 py-1 text-xs outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
                        <FontAwesomeIcon className="mr-2" icon={faUserPen} />Edit Profile
                     </button>
