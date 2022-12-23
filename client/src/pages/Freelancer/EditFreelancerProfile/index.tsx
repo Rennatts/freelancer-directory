25f2392cf295b58fee8792a52a345107d65292ab
Renata Machado <16.renata@gmail.com>
2022-12-23 17:41:02 -0300

    address edit

--- client/src/pages/Freelancer/EditFreelancerProfile/index.tsx
+++ client/src/pages/Freelancer/EditFreelancerProfile/index.tsx
@@ -21,11 +21,11 @@ export function EditFreelancerProfile ({ size= 'lg'}: IEditFreelancerProfileProp
         job_title: "",
         description: "",
         service_type: [""],
-        // zip_code: "",
-        // address: "",
-        // number: "",
-        // city: "",
-        // country: "",
+        zip_code: "",
+        address: "",
+        number: "",
+        city: "",
+        country: "",
     })
     
     let { freelancerId } = useParams();
@@ -37,26 +37,35 @@ export function EditFreelancerProfile ({ size= 'lg'}: IEditFreelancerProfileProp
       const fetchPositions = async () => {
         setIsLoading(true);
         await axios(`http://localhost:3000/api/freelancer/${freelancerId}`).then((response) => 
-        setUserData({job_title: response.data.job_title, description: response.data.description, service_type: response.data.service_type}))
+        setUserData({
+          job_title: response.data.job_title, 
+          description: response.data.description, 
+          service_type: response.data.service_type,
+          zip_code: response.data.zip_code,
+          address: response.data.address,
+          number:response.data.number,
+          city: response.data.city,
+          country: response.data.country,
+        }))
       };
 
       fetchPositions();
-    }, [selectedServices]);
+
+    }, []);
 
 
-    const handleSubmit = async (e: any) => {
+    const handleSubmit = (e: any) => {
         e.preventDefault();
+        console.log("hiiiiiiiiiii")
         const config = {
           headers: {
               'Content-Type': 'application/json',
               Authorization: `${context?.token}`,
           }
         };
-    
+
         axios.put(`http://localhost:3000/api/freelancer/${context?.id}`, userData, config)
         .then((res) => {
-          console.log("res", res)
-    
           if(res.status === 200){
             setSuccess(true)
             setTimeout(() => {
@@ -111,7 +120,8 @@ export function EditFreelancerProfile ({ size= 'lg'}: IEditFreelancerProfileProp
             ))}
           </div>
         </div>
-        {/* <div className="mb-6">
+        <div className="mb-6 mt-8">
+          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-cyan-500">Address</label>
           <div className="flex items-start flex-col mb-6">
               <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-cyan-500">zip code
               </label>
@@ -162,7 +172,7 @@ export function EditFreelancerProfile ({ size= 'lg'}: IEditFreelancerProfileProp
               onChange={(event)=> handleInputChange(event)}  className="bg-gray-50 border bg-transparent border-teal-500 text-gray-900 text-sm rounded-lg focus:ring-teal-300 focus:border-teal-300 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-teal-300" required>
               </input>
           </div>
-        </div> */}
+        </div>
         <button onClick={handleSubmit} className="flex-shrink-0 bg-teal-500 hover:bg-teal-300 hover:border-bg-teal-300 hover:text-black hover:border-teal-300 text-md text-white py-1 px-5 rounded" type="button">
           Save
         </button>
