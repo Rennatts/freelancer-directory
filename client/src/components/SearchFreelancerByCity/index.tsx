cfff87280e71e0e84b14a147d830501b41df27c0
Renata Machado <16.renata@gmail.com>
2023-01-15 12:04:28 -0300

    responsive search by city

--- client/src/components/SearchFreelancerByCity/index.tsx
+++ client/src/components/SearchFreelancerByCity/index.tsx
@@ -4,11 +4,13 @@ import './search.css';
 import {useNavigate } from "react-router-dom";
 import teamImage from './../../images/creative_team_re.svg';
 import axios from 'axios';
+import clsx from 'clsx';
 
 export interface ISearchProps {
+  size?: 'sm' | 'md' | 'lg';
 }
 
-export function SearchFreelancerByCity (props: ISearchProps) {
+export function SearchFreelancerByCity ({ size= 'sm' }: ISearchProps) {
   const [selectedCity, setSelectedCity] = React.useState("");
   const [FreelancerBySelectedCity, setFreelancerBySelectedCity] = React.useState([]);
   const navigate = useNavigate();
@@ -25,25 +27,53 @@ export function SearchFreelancerByCity (props: ISearchProps) {
 
   
   return (
-    <div className="h-80 py-52 flex items-center flex-center flex-col place-content-around mt-16 mb-64">
-      <div className="w-full flex flex-center items-center justify-center flex-col">
-        <p className='w-90 text-2xl mb-2 flex flex-center items-center justify-center'>Find a freelancer</p>
-        <p className="w-80 text-md text-gray-500">Some nice description about the plataform</p>
+    <div className={clsx(
+      'flex items-center align-center flex-center flex-col place-content-around',
+      {
+        '': size === 'sm',
+      }
+    )}>
+      <div className={clsx("w-full flex flex-center items-center justify-center flex-col", 
+      {
+
+      })}>
+        <p className={clsx('w-90 mb-2 flex flex-center items-center justify-center', 
+        {
+          'text-xl': size === 'sm',
+        })}>Find a freelancer</p>
+        <p className={clsx("text-gray-500", 
+        {
+          'text-sm w-40': size === 'sm',
+        })}>Some nice description about the plataform</p>
       </div>
-      <div className='mt-16'>
+      <div className='mt-6'>
         <form 
         onSubmit={handleSubmit}
-        className="w-full max-w-sm">
-          <div className="flex items-center border-b-2 border-teal-500 py-2">
+        className={clsx("w-full", 
+        {
+          'max-w-sm pl-6 pr-6': size === 'sm',
+        })}>
+          <div className={clsx("flex items-center border-b-2 border-teal-500", 
+          {
+            'mr-2 ml-2 py-2': size === 'sm',
+            'py-2': size === "lg",
+          })}>
             <input 
             id="input" 
             name='selectedCity'
             value={selectedCity}
             onChange={(event)=> setSelectedCity((event.target.value))}
-            className="appearance-none bg-transparent w-full mr-3 py-1 px-3 focus:outline-none text-md" type="text" placeholder="Search by city" aria-label="Full name"/>
+            className={clsx("appearance-none bg-transparent w-full mr-3 py-1 px-3 focus:outline-none", 
+            {
+              'text-xs': size === 'sm',
+            })} 
+            type="text" placeholder="Search by city" aria-label="Full name"/>
             <button 
             onClick={handleSubmit}
-            className="flex-shrink-0 bg-teal-500 hover:bg-teal-300 hover:border-bg-teal-300 hover:text-black hover:border-teal-300 text-md text-white py-1 px-5 rounded" type="button">
+            className={clsx("flex-shrink-0 bg-teal-500 hover:bg-teal-300 hover:border-bg-teal-300 hover:text-black hover:border-teal-300 text-white py-1 px-5 rounded", 
+            {
+              'mr-35 text-xs': size === 'sm',
+            })} type="button">
               Search
             </button>
           </div>
