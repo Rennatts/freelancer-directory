9a73bd43d21449423fc92e04eb9fa4300f3f37a9
Renata Machado <16.renata@gmail.com>
2023-02-11 17:58:19 -0300

    description changed

--- client/src/components/Description/index.tsx
+++ client/src/components/Description/index.tsx
@@ -12,19 +12,24 @@ export interface IDescriptionProps {
 
 export function Description (props: IDescriptionProps) {
   return (
-    <div className='photo_box'>
-      <div className='box_01'>
-        <img className='photo01' src={photo01} alt="photo01"></img>
-      </div>
-      <div className='box_02'>
-        <img className='photo02' src={photo02} alt="photo02"></img>
-      </div>
-      <div className='box_03'>
-        <img className='photo03' src={photo03} alt="photo03"></img>
-      </div>
-      <div className='description'>
+    <div className='bg-teal-700 pt-14 pl-4 pr-4 md:pt-2 md:pb-8'>
+      <div className='sm:grid sm:grid-rows-3 sm:grid-flow-col sm:gap-1 sm:m-20 grid grid-rows-2 grid-flow-col gap-1'>
+        <div className='sm:row-start-2 sm:row-span-2 row-span-1'>
+          <img className='' src={photo01} alt="photo01"></img>
+        </div>
+        <div className='sm:row-end-3 sm:row-span-2 col-span-2'>
+          <img className='' src={photo02} alt="photo02"></img>
+        </div>
+        <div className='sm:row-start-2 sm:row-span-2 row-span-2 col-span-2'>
+          <img className='' src={photo03} alt="photo03"></img>
+        </div>
+        <div className='row-span-3 sm:row-start-1 sm:row-span-4 text-white sm:text-md md:text-md lg:text-xl md:mt-10 md:mr-32 md:ml-32'>
         <p>Easily source top-notch freelancers, craft well-defined work agreements, track progress collaboratively, and ensure smooth communication throughout your project</p>
+        </div>
       </div>
+      {/* <div className='text-md mb-2 text-white flex flex-wrap flex-center items-center justify-center sm:text-md md:text-md lg:text-xl md:mt-10 md:mr-32 md:ml-32'>
+        <p>Easily source top-notch freelancers, craft well-defined work agreements, track progress collaboratively, and ensure smooth communication throughout your project</p>
+      </div> */}
     </div>
   );
 }
